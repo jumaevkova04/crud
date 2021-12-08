9fcf94eace6ea8fd28002bac25895d630d168bd1
jumaevkova04 <jumaevkovad@gmail.com>
2021-12-08 17:18:43 +0500

    initial version

---
+++ cmd/app/server.go
@@ -0,0 +1,210 @@
+package app
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/jumaevkova04/crud/pkg/customers"
+)
+
+// Server ...
+type Server struct {
+	mux          *http.ServeMux
+	customersSvc *customers.Service
+}
+
+// NewServer ...
+func NewServer(mux *http.ServeMux, customersSvc *customers.Service) *Server {
+	return &Server{mux: mux, customersSvc: customersSvc}
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.mux.ServeHTTP(w, r)
+}
+
+func (s *Server) Init() {
+	s.mux.HandleFunc("/customers.getAll", s.handleGetAllCustomers)
+	s.mux.HandleFunc("/customers.getAllActive", s.handleGetAllActiveCustomers)
+	s.mux.HandleFunc("/customers.getById", s.handleGetCustomerByID)
+	s.mux.HandleFunc("/customers.save", s.handleSaveCustomer)
+	s.mux.HandleFunc("/customers.removeById", s.handleRemoveByID)
+	s.mux.HandleFunc("/customers.blockById", s.handleBlockByID)
+	s.mux.HandleFunc("/customers.unblockById", s.handleUnblockByID)
+}
+
+func (s *Server) handleGetCustomerByID(w http.ResponseWriter, r *http.Request) {
+	idParam := r.URL.Query().Get("id")
+
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	item, err := s.customersSvc.ByID(r.Context(), id)
+	if errors.Is(err, customers.ErrNotFound) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = sendResponse(w, item)
+	if err != nil {
+		log.Println("ERROR", err)
+	}
+}
+
+func (s *Server) handleGetAllCustomers(w http.ResponseWriter, r *http.Request) {
+	items, err := s.customersSvc.All(r.Context())
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = sendResponse(w, items)
+	if err != nil {
+		log.Println("ERROR", err)
+	}
+}
+
+func (s *Server) handleGetAllActiveCustomers(w http.ResponseWriter, r *http.Request) {
+	items, err := s.customersSvc.AllActive(r.Context())
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = sendResponse(w, items)
+	if err != nil {
+		log.Println("ERROR", err)
+	}
+}
+
+func (s *Server) handleSaveCustomer(w http.ResponseWriter, r *http.Request) {
+	idParam := r.FormValue("id")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	name := r.FormValue("name")
+	phone := r.FormValue("phone")
+
+	var customer = &customers.Customer{
+		ID:      id,
+		Name:    name,
+		Phone:   phone,
+		Active:  "",
+		Created: time.Time{},
+	}
+
+	item, err := s.customersSvc.Save(r.Context(), customer)
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = sendResponse(w, item)
+	if err != nil {
+		log.Println("ERROR", err)
+	}
+}
+
+func (s *Server) handleRemoveByID(w http.ResponseWriter, r *http.Request) {
+	idParam := r.URL.Query().Get("id")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	item, err := s.customersSvc.RemoveByID(r.Context(), id)
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = sendResponse(w, item)
+	if err != nil {
+		log.Println("ERROR", err)
+		return
+	}
+}
+
+func (s *Server) handleBlockByID(w http.ResponseWriter, r *http.Request) {
+	idParam := r.URL.Query().Get("id")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	item, err := s.customersSvc.BlockByID(r.Context(), id)
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = sendResponse(w, item)
+	if err != nil {
+		log.Println("ERROR", err)
+		return
+	}
+}
+
+func (s *Server) handleUnblockByID(w http.ResponseWriter, r *http.Request) {
+	idParam := r.URL.Query().Get("id")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	item, err := s.customersSvc.UnblockByID(r.Context(), id)
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = sendResponse(w, item)
+	if err != nil {
+		log.Println("ERROR", err)
+		return
+	}
+}
+
+func sendResponse(w http.ResponseWriter, response interface{}) error {
+	data, err := json.Marshal(response)
+	if err != nil {
+		log.Println("ERROR", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(data)
+	if err != nil {
+		log.Println("ERROR", err)
+		return err
+	}
+	return nil
+}
---
+++ pkg/customers/service.go
@@ -0,0 +1,227 @@
+package customers
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"log"
+	"time"
+)
+
+var (
+	ErrNotFound = errors.New("item not found")
+	ErrInternal = errors.New("internal error")
+)
+
+// Service ...
+type Service struct {
+	db *sql.DB
+}
+
+func NewService(db *sql.DB) *Service {
+	return &Service{db: db}
+}
+
+// Customer ...
+type Customer struct {
+	ID      int64     `json:"id"`
+	Name    string    `json:"name"`
+	Phone   string    `json:"phone"`
+	Active  string    `json:"active"`
+	Created time.Time `json:"created"`
+}
+
+// All ...
+func (s *Service) All(ctx context.Context) ([]*Customer, error) {
+	items := make([]*Customer, 0)
+
+	rows, err := s.db.QueryContext(ctx, `
+	SELECT id, name, phone, active, created FROM customers;
+	`)
+	if err != nil {
+		log.Println("ERROR", err)
+		return nil, ErrInternal
+	}
+	defer func() {
+		if cerr := rows.Close(); cerr != nil {
+			if err == nil {
+				err = cerr
+			}
+			log.Println("ERROR", cerr)
+		}
+	}()
+
+	for rows.Next() {
+		var item = &Customer{}
+
+		err = rows.Scan(&item.ID, &item.Name, &item.Phone, &item.Active, &item.Created)
+		if err != nil {
+			log.Println("ERROR", err)
+			return nil, ErrInternal
+		}
+		items = append(items, item)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Println("ERROR", err)
+		return nil, ErrInternal
+	}
+
+	return items, nil
+}
+
+// All ...
+func (s *Service) AllActive(ctx context.Context) ([]*Customer, error) {
+	items := make([]*Customer, 0)
+
+	rows, err := s.db.QueryContext(ctx, `
+	SELECT id, name, phone, active, created FROM customers WHERE active;
+	`)
+	if err != nil {
+		log.Println("ERROR", err)
+		return nil, ErrInternal
+	}
+	defer func() {
+		if cerr := rows.Close(); cerr != nil {
+			if err == nil {
+				err = cerr
+			}
+			log.Println("ERROR", cerr)
+		}
+	}()
+
+	for rows.Next() {
+		var item = &Customer{}
+
+		err = rows.Scan(&item.ID, &item.Name, &item.Phone, &item.Active, &item.Created)
+		if err != nil {
+			log.Println("ERROR", err)
+			return nil, ErrInternal
+		}
+		items = append(items, item)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Println("ERROR", err)
+		return nil, ErrInternal
+	}
+
+	return items, nil
+}
+
+// ByID ...
+func (s *Service) ByID(ctx context.Context, id int64) (*Customer, error) {
+	item := &Customer{}
+
+	err := s.db.QueryRowContext(ctx, `
+	SELECT id, name, phone, active, created FROM customers WHERE id = $1;
+	`, id).Scan(&item.ID, &item.Name, &item.Phone, &item.Active, &item.Created)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, ErrNotFound
+	}
+
+	if err != nil {
+		log.Println("ERROR", err)
+		return nil, ErrInternal
+	}
+
+	return item, nil
+}
+
+// Save ...
+func (s *Service) Save(ctx context.Context, item *Customer) (*Customer, error) {
+	var customer = &Customer{}
+
+	if item.ID == 0 {
+		err := s.db.QueryRowContext(ctx, `
+		INSERT INTO customers (name, phone) VALUES($1, $2) RETURNING id, name, phone, active, created;
+		`, item.Name, item.Phone).Scan(&customer.ID, &customer.Name, &customer.Phone, &customer.Active, &customer.Created)
+
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+
+		if err != nil {
+			log.Println("ERROR", err)
+			return nil, ErrInternal
+		}
+	} else {
+		err := s.db.QueryRowContext(ctx, `
+		UPDATE customers SET name = $1, phone = $2 RETURNING id, name, phone, active, created;
+		`, item.Name, item.Phone).Scan(&customer.ID, &customer.Name, &customer.Phone, &customer.Active, &customer.Created)
+
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+
+		if err != nil {
+			log.Println("ERROR", err)
+			return nil, ErrInternal
+		}
+	}
+
+	return customer, nil
+}
+
+// RemoveByID ...
+func (s *Service) RemoveByID(ctx context.Context, id int64) (*Customer, error) {
+	var customer = &Customer{}
+
+	err := s.db.QueryRowContext(ctx, `
+	DELETE FROM customers WHERE id = $1 RETURNING id, name, phone, active, created;
+	`, id).Scan(&customer.ID, &customer.Name, &customer.Phone, &customer.Active, &customer.Created)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, ErrNotFound
+	}
+
+	if err != nil {
+		log.Println("ERROR", err)
+		return nil, ErrInternal
+	}
+
+	return customer, nil
+}
+
+// BlockByID ...
+func (s *Service) BlockByID(ctx context.Context, id int64) (*Customer, error) {
+	var customer = &Customer{}
+
+	err := s.db.QueryRowContext(ctx, `
+		UPDATE customers SET active = $2 WHERE id = $1 RETURNING id, name, phone, active, created
+		`, id, false).Scan(&customer.ID, &customer.Name, &customer.Phone, &customer.Active, &customer.Created)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, ErrNotFound
+	}
+
+	if err != nil {
+		log.Println("ERROR", err)
+		return nil, ErrInternal
+	}
+
+	return customer, nil
+}
+
+// UnblockByID ...
+func (s *Service) UnblockByID(ctx context.Context, id int64) (*Customer, error) {
+	var customer = &Customer{}
+
+	err := s.db.QueryRowContext(ctx, `
+		UPDATE customers SET active = $2 WHERE id = $1 RETURNING id, name, phone, active, created
+		`, id, true).Scan(&customer.ID, &customer.Name, &customer.Phone, &customer.Active, &customer.Created)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, ErrNotFound
+	}
+
+	if err != nil {
+		log.Println("ERROR", err)
+		return nil, ErrInternal
+	}
+
+	return customer, nil
+}
