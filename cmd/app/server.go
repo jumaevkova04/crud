da3f749190df84dbb9d304fbe8ba49d845548051
jumaevkova04 <jumaevkovad@gmail.com>
2021-12-10 17:27:43 +0500

    modified

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -276,6 +276,7 @@ func sendResponse(w http.ResponseWriter, response interface{}) error {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	_, err = w.Write(data)
 	if err != nil {
 		log.Println("ERROR", err)
